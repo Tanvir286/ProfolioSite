63f59a698d0a5b9c1c9509ad48af1e8d0f08fc7c
Tanvir286 <tanvirdiu200@gmail.com>
2024-10-15 17:04:34 +0600

    ok

--- src/views/projects/projects.js
+++ src/views/projects/projects.js
@@ -80,7 +80,7 @@ export default function Projects() {
             projectImage={School}
             projectName={"ACP HIGH SCHOOL"}
             aboutProject={"The website is focused on a high school environment."}
-            techStack={"REACT JS, TALIWIND CSS ,EXPRESS JS, MONGODB"}
+            techStack={"HTML,CSS,JAVASCRIPT"}
             projectLiveLink="https://tanvir286.github.io/School-website/"
             projectLink="https://github.com/Tanvir286/School-website"
           />
